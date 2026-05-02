16ba269647e74e021d241392777f65e5abe8c815
ssb-shree <bavachikarshree1666@gmail.com>
2026-05-02 23:02:11 +0530

    room type update

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -2,14 +2,24 @@ export type RoomType = {
   id: string;
   players: PlayerType[];
   owner: PlayerType;
-  guessWord: string;
-  canvasState: any;
-  status: "started" | "waiting";
+  currentWord: string;
+  guessedPlayers: Set<string>; // socket ID
+  toTakeTurn: PlayerType[]; // socket ID's
+  turnIndex: number;
+  scores: Record<string, number>;
+  canvasState: LineType[];
+  status: "started" | "waiting" | "ended";
+  turnTimer?: NodeJS.Timeout;
 };
 
 export type PlayerType = {
   id: string;
   username: string;
   score: number;
-  inRoom: string;
+};
+
+export type LineType = {
+  points: number[];
+  color: string;
+  width: number;
 };
